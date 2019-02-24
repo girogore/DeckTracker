8aa5b1194cfde721d3ef82a5d4517c7a1a4d104e
girogore <girogore@users.noreply.github.com>
2019-02-23 22:27:57 -0800

    Initial commit

---
+++ src/main/java/DeckTracker/Core/DeckTracker.java
@@ -0,0 +1,117 @@
+package DeckTracker.Core;
+
+import basemod.BaseMod;
+import basemod.abstracts.CustomPlayer;
+import basemod.interfaces.PostDrawSubscriber;
+import basemod.interfaces.StartGameSubscriber;
+import basemod.interfaces.OnStartBattleSubscriber;
+import basemod.interfaces.OnCardUseSubscriber;
+import basemod.interfaces.PostBattleSubscriber;
+import com.badlogic.gdx.graphics.g2d.*;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.badlogic.gdx.graphics.Texture;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+@SpireInitializer
+public class DeckTracker implements PostDrawSubscriber, StartGameSubscriber, OnStartBattleSubscriber, OnCardUseSubscriber, PostBattleSubscriber {
+    private TreeMap<AbstractCard, Integer> deckList;
+    public static final Logger logger = LogManager.getLogger(DeckTracker.class.getName());
+    ArrayList<DeckTrackerCard> cardList;
+
+    public DeckTracker() {
+        cardList = new ArrayList<DeckTrackerCard>();
+        BaseMod.subscribe(this);
+    }
+
+    public static void initialize() {
+        DeckTracker tracker = new DeckTracker();
+    }
+
+
+    @Override
+    public void receiveStartGame() {
+    }
+
+    @Override
+    public void receivePostDraw(AbstractCard c) {
+        Update();
+    }
+
+    @Override
+    public void receiveCardUsed(AbstractCard card) {
+        Update();
+    }
+
+    @Override
+    public void receiveOnBattleStart(AbstractRoom abstractRoom) {
+        Update();
+    }
+
+    @Override
+    public void receivePostBattle(AbstractRoom abstractRoom) {
+        ResetList();
+    }
+
+    private void ResetList() {
+        for (DeckTrackerCard dt : cardList) {
+            dt.Remove();
+        }
+        cardList.clear();
+    }
+
+    public void Update() {
+        try {AbstractDungeon.getCurrRoom();}
+        catch (Exception e) { logger.error("You played a card while not in combat?");}
+        ResetList();
+        Texture COLORLESS_ORB = ImageMaster.loadImage("img/DeckTracker_colorless_orb.png");
+        int y = 0;
+        TextureRegion TROrb = null;
+        deckList = getDeckList();
+        for (Map.Entry<AbstractCard, Integer> entry : deckList.entrySet()) {
+            int amount = entry.getValue();
+            --y;
+            AbstractCard card = entry.getKey();
+
+            // Orbs
+            switch (card.color) {
+                case RED:
+                    TROrb = new TextureRegion(ImageMaster.RED_ORB, 0, 0, ImageMaster.RED_ORB.packedWidth, ImageMaster.RED_ORB.packedHeight);
+                    break;
+                case BLUE:
+                    TROrb = new TextureRegion(ImageMaster.BLUE_ORB, 0, 0, ImageMaster.BLUE_ORB.packedWidth, ImageMaster.BLUE_ORB.packedHeight);
+                    break;
+                case GREEN:
+                    TROrb = new TextureRegion(ImageMaster.GREEN_ORB, 0, 0, ImageMaster.GREEN_ORB.packedWidth, ImageMaster.GREEN_ORB.packedHeight);
+                    break;
+                case CURSE:
+                case COLORLESS:
+                    TROrb = new TextureRegion(COLORLESS_ORB, 0, 0, COLORLESS_ORB.getWidth(), COLORLESS_ORB.getHeight());
+                    break;
+                default:
+                    TextureAtlas.AtlasRegion energyOrbAR = ((CustomPlayer) AbstractDungeon.player).getOrb();
+                    TROrb = new TextureRegion(energyOrbAR, 0, 0, energyOrbAR.packedWidth, energyOrbAR.packedHeight);
+                    break;
+            }
+            DeckTrackerCard dtCard = new DeckTrackerCard(card, TROrb, y, amount);
+            cardList.add(dtCard);
+        }
+    }
+
+    private TreeMap<AbstractCard, Integer> getDeckList() {
+        TreeMap<AbstractCard, Integer> ret = new TreeMap<>();
+        for (AbstractCard card : AbstractDungeon.player.drawPile.group)  {
+            ret.put(card, ret.getOrDefault(card, 0) + 1);
+        }
+        return ret;
+    }
+}
---
+++ src/main/java/DeckTracker/Core/DeckTrackerCard.java
@@ -0,0 +1,201 @@
+package DeckTracker.Core;
+
+import basemod.BaseMod;
+import basemod.interfaces.PreRoomRenderSubscriber;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.FontHelper;
+import com.megacrit.cardcrawl.helpers.Hitbox;
+import com.megacrit.cardcrawl.helpers.TipHelper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import basemod.abstracts.DynamicVariable;
+
+public class DeckTrackerCard implements PreRoomRenderSubscriber {
+    AbstractCard card;
+    Hitbox hb;
+    float index;
+    int amount;
+    float OFFSET = (float) Settings.HEIGHT - (160.0F * Settings.scale);
+    private static int rowHeight = 30;
+    private static int rowWidth = 200;
+    float xloc = 6*Settings.scale;
+    float width = rowWidth * Settings.scale;
+    float height = rowHeight * Settings.scale;
+    TextureRegion orbTexture;
+    String cost;
+    String name;
+    String description;
+    BitmapFont titleFont;
+    public static final Logger logger = LogManager.getLogger(DeckTracker.class.getName());
+
+
+
+    public DeckTrackerCard(AbstractCard card, TextureRegion orbTR, int y, int amount) {
+        this.card = card;
+        index = (y * rowHeight * 1.1F * Settings.scale) + (OFFSET);
+        this.amount = amount;
+        orbTexture = orbTR;
+
+        if (card.cost < 0) cost = "-";
+        else cost = Integer.toString(card.cost);
+        name = card.name;
+
+        titleFont = FontHelper.cardTitleFont_N;
+        FontHelper.menuBannerFont.getData().setScale(0.7F);
+
+        hb = new Hitbox(xloc, index, width, height);
+        BaseMod.subscribe(this);
+    }
+
+    public void Remove()
+    {
+        BaseMod.unsubscribe(this);
+    }
+
+    @Override
+    public void receivePreRoomRender(SpriteBatch sb) {
+        this.hb.update();
+        if (this.hb.justHovered) {
+            try {
+                UpdateDescription();
+            }
+            catch (Exception e) {
+                description = card.rawDescription;
+            }
+        }
+
+        if (this.hb.hovered) {
+            TipHelper.renderGenericTip(width+ 15.0F, index, this.name, description);
+        }
+
+        sb.setColor(Color.WHITE.cpy());
+
+        try {
+            sb.draw(orbTexture, width, index, height, height);
+            TextureAtlas.AtlasRegion AR = card.portrait;
+            TextureRegion TR = new TextureRegion(AR, 0, (AR.packedHeight / 3), AR.packedWidth, AR.packedHeight / 3);
+            sb.draw(TR, xloc, index, width, height);
+        }
+        catch (Exception e) {
+            logger.error("Decktracker:: card - " + name + " failed to draw.");
+        }
+
+        titleFont.getData().setScale(1.0F);
+        FontHelper.renderFont(sb, FontHelper.menuBannerFont, Integer.toString(amount), 5, index+(2.0F * Settings.scale)+(height*0.7F), Color.GOLD);
+        titleFont.getData().setScale(0.7F);
+        FontHelper.renderFont(sb, titleFont, name, 20, index+(height*0.7F), Color.WHITE);
+        titleFont.getData().setScale(0.8F);
+        FontHelper.renderFont(sb, titleFont, cost, width+(height*0.35F), index+(height*0.7F), Color.WHITE);
+
+    }
+
+
+    //https://github.com/bug-sniper/card-channeler-mod/blob/master/src/main/java/cardchanneler/orbs/ChanneledCard.java
+    //Author::bug-sniper
+    private String getDynamicValue(final String key) {
+        String value = null;
+        if (key.length() == 1){
+            switch (key.charAt(0)) {
+                case 'B': {
+                    if (!card.isBlockModified) {
+                        return Integer.toString(card.baseBlock);
+                    }
+                    if (card.block >= card.baseBlock) {
+                        return "[#7fff00]" + Integer.toString(card.block) + "[]";
+                    }
+                    return "[#ff6563]" + Integer.toString(card.block) + "[]";
+                }
+                case 'D': {
+                    if (!card.isDamageModified) {
+                        return Integer.toString(card.baseDamage);
+                    }
+                    if (card.damage >= card.baseDamage) {
+                        return "[#7fff00]" + Integer.toString(card.damage) + "[]";
+                    }
+                    return "[#ff6563]" + Integer.toString(card.damage) + "[]";
+                }
+                case 'M': {
+                    if (!card.isMagicNumberModified) {
+                        return Integer.toString(card.baseMagicNumber);
+                    }
+                    if (card.magicNumber >= card.baseMagicNumber) {
+                        return "[#7fff00]" + Integer.toString(card.magicNumber) + "[]";
+                    }
+                    return "[#ff6563]" + Integer.toString(card.magicNumber) + "[]";
+                }
+                default: {
+                    logger.info("KEY: " + key);
+                    return Integer.toString(-99);
+                }
+            }
+        }
+        else {
+            DynamicVariable dv = BaseMod.cardDynamicVariableMap.get(key);
+            if (dv != null) {
+                if (dv.isModified(card)) {
+                    if (dv.value(card) >= dv.baseValue(card)) {
+                        value = "[#" + dv.getIncreasedValueColor().toString() + "]" + Integer.toString(dv.value(card)) + "[]";
+                    } else {
+                        value = "[#" + dv.getDecreasedValueColor().toString() + "]" + Integer.toString(dv.value(card)) + "[]";
+                    }
+                } else {
+                    value = Integer.toString(dv.baseValue(card));
+                }
+            }
+            logger.info(key + " is " + value);
+            return (String) value;
+        }
+    }
+
+    //https://github.com/bug-sniper/card-channeler-mod/blob/master/src/main/java/cardchanneler/orbs/ChanneledCard.java
+    //Author::bug-sniper
+    // Set the on-hover description of the card
+    public void UpdateDescription() {
+        description = "";
+        boolean firstWord = true;
+        card.initializeDescription();
+        String descriptionFragment = "";
+        for (int i=0; i<card.description.size(); i++){
+            descriptionFragment = card.description.get(i).text;
+            for (String word : descriptionFragment.split(" ")) {
+                if (firstWord){
+                    firstWord = false;
+                }else{
+                    description += " ";
+                }
+                if (word.length() > 0 && word.charAt(0) == '*') {
+                    word = word.substring(1);
+                    String punctuation = "";
+                    if (word.length() > 1 && !Character.isLetter(word.charAt(word.length() - 2))) {
+                        punctuation += word.charAt(word.length() - 2);
+                        word = word.substring(0, word.length() - 2);
+                        punctuation += ' ';
+                    }
+                    description += word;
+                    description += punctuation;
+                }
+                else if (word.length() > 0 && word.charAt(0) == '!') {
+                    String key = "";
+                    for (int j=1; j<word.length(); j++){
+                        if (word.charAt(j) == '!'){
+                            description += getDynamicValue(key);
+                            description += word.substring(j+1);
+                        }
+                        else {
+                            key += word.charAt(j);
+                        }
+                    }
+                }
+                else{
+                    description += word;
+                }
+            }
+        }
+    }
+}
